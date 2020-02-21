6c0774753a1ed0d88edf92eec5cd7eb80be7cf27
Eshita Banerjee <ebanerje@andrew.cmu.edu>
2020-02-20 22:13:31 -0500

    finished hw5

--- wk5 starter files/main.js
+++ wk5 starter files/main.js
@@ -9,3 +9,16 @@ function addNewList() {
     console.log('hello world console');
 }
 
+function addListItem() {
+console.log('hello world');
+}
+
+var list = document.getElementById("grocery-list");
+
+var itemInput = document.getElementById("new-list-item");
+
+var newItem = document.createElement("li");
+
+newItem.appendChild(document.createTextNode(itemInput.value));
+
+list.appendChild(newItem);
